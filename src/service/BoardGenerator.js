1ab51430cae4e11001b2e96cfec1cd458a578944
luckmer <woszczo04@gmail.com>
2021-11-05 12:32:08 +0100

    component to display area

--- src/service/BoardGenerator.js
+++ src/service/BoardGenerator.js
@@ -1,41 +1,11 @@
 import { useState, useEffect, useMemo } from 'react';
-import boardMap from './Board';
+import Generator from './Generator';
 
 const ChessMapGenerator = () => {
   const [board, setBoard] = useState([]);
 
   useEffect(() => {
-    const boardSize = 8;
-    let index = 0;
-    let black = false;
-
-    const box = [];
-
-    for (let i = 1; i <= boardSize ** 2; i++) {
-      let IdColor = null;
-      if (black) {
-        IdColor = 1;
-        index++;
-        black = !black;
-      } else {
-        IdColor = 0;
-        index++;
-        black = !black;
-      }
-
-      const grid = {
-        id: i,
-        _id: i,
-        background: IdColor,
-        Img: boardMap[i - 1][0],
-        type: boardMap[i - 1][1]
-      };
-      box.push(grid);
-      if (index === 8) {
-        black = !black;
-        index = 0;
-      }
-    }
+    const box = Generator();
     setBoard(box);
   }, []);
 
