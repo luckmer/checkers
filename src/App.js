bb3a32e261f29a7337c18c8848c483a86d3463b1
luckmer <woszczo04@gmail.com>
2021-11-05 12:33:33 +0100

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
-import { useState, useEffect } from 'react';
-import { CheckersSection, Div } from './css/CheckersSection.Style';
-
-import DragAndDrop from './hooks/helper/drop/DragAndDrop';
-import ChessMapGenerator from './service/BoardGenerator';
-import helper from './constants/helper';
+import { Routes, Route } from 'react-router';
+import { routes } from './routes/index';
 
 const App = () => {
-  const [gameOver, setGameOver] = useState(false);
-  const [currentPlayer, setCurrentPlayer] = useState('white');
-
-  const { boardData, setBoard } = ChessMapGenerator();
-
-  useEffect(() => {
-    if (!boardData.length) return;
-
-    const arr = boardData;
-
-    const getPawns = arr.filter(({ type }) => type).map(({ type }) => type);
-
-    const createType = getPawns.map((type) => helper.queenType(type));
-    const map = {};
-
-    createType.forEach((el) => (map[el] = (map[el] || 0) + 1));
-  }, [boardData]);
-
-  const { handleDragOver, handleDragStart, handleDrop } = DragAndDrop(
-    boardData,
-    currentPlayer,
-    setBoard,
-    setCurrentPlayer
-  );
-
   return (
-    <CheckersSection>
-      {boardData.map((el, i) => {
-        const check = el.Img === 'Empty';
-
-        return (
-          <Div
-            key={i}
-            type={el.type}
-            id={el.id}
-            onDragOver={(e) => handleDragOver(e)}
-            onDragStart={(e) => (check ? null : handleDragStart(e))}
-            onDrop={(e) => handleDrop(e)}
-            Design={el.background}
-            IMG={el.Img}
-            draggable={check ? null : true}
-          ></Div>
-        );
-      })}
-    </CheckersSection>
+    <Routes>
+      {routes.map(({ path, Element }) => (
+        <Route key={path} path={path} element={<Element />} />
+      ))}
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import GlobalStyle from "./css/Global.Style";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './css/Global.Style';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
     <GlobalStyle />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
